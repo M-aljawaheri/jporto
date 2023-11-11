55642352ad497760fe616b395d5487915806e604
mjawaher <m_aljawaheri@outlook.com>
2023-11-12 00:27:00 +0300

    Personal website, First commit

---
+++ src/app/animatedTags.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
+
+const SpookyText = () => {
+  let text: string = "Hello stranger";
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((count) => count+1 % text.length);
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+  return (<h1 className="text-xl font-burtons">{ text.substring(count, text.length) + text.substring(0, count) }</h1>);
+}
+
+export default SpookyText;
